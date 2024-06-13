5fcb0456e825b87d54aac954fbbb52fb007d2127
moein <moein.baghsheikhi@gmail.com>
2024-06-13 07:53:14 +0330

    fix bot

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const { Telegraf } = require('telegraf')
+const { Telegraf, Markup } = require('telegraf')
 const bot = new Telegraf("7236215598:AAHiMnspxnk2ead1RcHHPvGyQ-BMhlTeG68")
 
 bot.start((ctx) => {
-    console.log(ctx)
-    ctx.reply("Welcome to GPT-4!")
+    ctx.reply("Welcome to GPT-4!",
+        Markup.keyboard([
+            Markup.button.callback('help')
+        ])
+    )
 })
 
 bot.launch();
